b415bb7870f9e76deffa106e2889480a5385c64b
AlbertoOrlando <albertoorlandowork@google.com>
2025-04-23 16:50:39 +0200

    Remove unnecessary blank line in countdown function

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,7 +1,6 @@
 function contoAllaROvescia(num) {
     const intervallo = setInterval(() => {
         console.log("il contatore è a " + num);
-
         if (num == 0) {
             clearInterval(intervallo)
             console.log("conto alla rovescia terminato");
