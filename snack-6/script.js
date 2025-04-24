b98595f83c3f50f8b432ae8f7606a701c1a0c8d1
AlbertoOrlando <albertoorlandowork@google.com>
2025-04-24 10:09:04 +0200

    Refactor creaContatoreAutomatico to return a function that manages the interval and timeout for the counter

--- snack-6/script.js
+++ snack-6/script.js
@@ -1,15 +1,19 @@
 function creaContatoreAutomatico(time) {
     let contatore = 0
-    return setInterval(() => {
-        contatore += 1;
-        console.log("contatore " + contatore)
-    }, time)
+    return () => {
+        const intervallo = setInterval(() => {
+            contatore += 1;
+            console.log("contatore " + contatore)
+        }, time)
+        setTimeout(() => {
+            clearInterval(intervallo); console.log("contatore fermato");
+        }, 10000)
+    }
 }
 
 const tempoDiAumento = creaContatoreAutomatico(2000)
+tempoDiAumento()
 
-setTimeout(() => {
-    clearInterval(tempoDiAumento); console.log("contatore fermato");
-}, 10000)
+
 
 
